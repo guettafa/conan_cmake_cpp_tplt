b5f799cb3da89f92e3388afa583bb63a5d601a18
guettafa <irontitou5@gmail.com>
2024-10-18 14:53:16 -0400

    Added simple test

---
+++ include/utils.h
@@ -0,0 +1,12 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+namespace Utils {
+  
+  template<typename T> 
+  inline T AddTogether(T val1, T val2) {
+    return val1 + val2;
+  }
+}
+
+#endif // UTILS_H
--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <gtest/gtest.h>
 
+#include "utils.h"
+
+TEST(AddTogetherTest, TestingFunction) {
+  EXPECT_EQ(3, Utils::AddTogether<int>(1, 2));
+}
+
 int main(int argc, char* argv[]) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
